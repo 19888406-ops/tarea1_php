cd7cff1192611e0da00a89dd31e3f3ea46ba7513
19888406-ops <19888406@clases.edu.sv>
2026-05-18 21:20:41 -0600

    ejercicio18
    en este ejercicio realize un formulario donde  puse el nombre del cliente el producto la cantidad y el precio

---
+++ ejercicio18.php
@@ -0,0 +1,16 @@
+<?php
+
+$cliente=$_POST['cliente'];
+$producto=$_POST['producto'];
+$cantidad=$_POST['cantidad'];
+$precio=$_POST['precio'];
+
+$subtotal=$cantidad*$precio;
+$total=$subtotal; 
+
+echo "Cliente: ".$cliente."<br>";
+echo "Producto: ".$producto."<br>";
+echo "Subtotal: $".$subtotal."<br>";
+echo "Total a pagar: $".$total;
+
+?>
