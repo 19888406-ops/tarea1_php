4c2d0ad23912ad8413d078398badc37226c27814
19888406-ops <19888406@clases.edu.sv>
2026-05-18 19:40:27 -0600

    ejercicio10
    en este ejercicio puse tres notas donde se calcule y muestre el periodo

---
+++ ejercicio10.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero1=$_POST['n1'];
+$numero2=$_POST['n2'];
+$numero3=$_POST['n3'];
+
+$promedio=($numero1+$numero2+$numero3)/3;
+
+echo "El promedio del estudiante es: ".$promedio;
+
+?>
